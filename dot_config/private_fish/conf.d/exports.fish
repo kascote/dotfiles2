e8c5ff5d2ec6fd117a9e9a82e5d7515f073d1edd
nelson fernandez <nelson@netflux.com.ar>
2022-07-30 13:01:50 -0300

    update config

--- dot_config/private_fish/conf.d/exports.fish
+++ dot_config/private_fish/conf.d/exports.fish
@@ -10,7 +10,9 @@ set -gx LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
 
 set -x ZF_DEFAULT_COMMAND "fd --type file --color=always"
 # set -x ansi --bind ctrl-f:page-down,ctrl-b:page-up --color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81 --color info:144,prompt:161,spinner:135,pointer:135,marker:118"
-set -x FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS' --ansi --bind ctrl-f:page-down,ctrl-b:page-up --color=fg:#8b949e,bg:#0d1117,hl:#e3b341 --color=fg+:#eeeeee,bg+:#0d419d,hl+:#d29922 --color=info:#87ff00,prompt:#d7005f,pointer:#af5fff --color=marker:#79c0ff,spinner:#af5fff,header:#f0883e'
+# set -x FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS' --ansi --bind ctrl-f:page-down,ctrl-b:page-up --color=fg:#8b949e,bg:#0d1117,hl:#e3b341 --color=fg+:#eeeeee,bg+:#0d419d,hl+:#d29922 --color=info:#87ff00,prompt:#d7005f,pointer:#af5fff --color=marker:#79c0ff,spinner:#af5fff,header:#f0883e'
+# catppuccin
+set -x FZF_DEFAULT_OPTS "--ansi --bind ctrl-f:page-down,ctrl-b:page-up --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
 set -gx DART_SDK "$HOMEBREW_PREFIX/opt/dart"
